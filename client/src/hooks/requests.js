2ba1f3837dbb7b4244c32262247da68880d9525a
ashadbubt <ashad.feni@gmail.com>
2022-08-27 20:16:48 +0600

    complete nasa project

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,24 +1,45 @@
 
-const API_URL = 'http://localhost:8000/'
+const API_URL = 'http://localhost:8000'
 async function httpGetPlanets() {
 
- const response=  await fetch(`${ API_URL }planets`);
+ const response=  await fetch(`${ API_URL }/planets`);
  return await response.json();
 }
 
 async function httpGetLaunches() {
   // TODO: Once API is ready.
+  const response = await fetch(`${API_URL}/launches`);
+  return await response.json()
   // Load launches, sort by flight number, and return as JSON.
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try{ 
+      return await fetch(`${API_URL}/launches`, {
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch(err){
+    return {
+      ok:false
+    }
+  }
 }
 
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try{
+    return await fetch(`${API_URL}/launches/${id}`,{
+      method:"delete",
+    })
+  } catch(err){
+    console.log(err);
+    return {
+      ok:false
+    }
+  }
 }
 
 export {
--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,29 @@
+const path = require('path');
 const express = require('express');
-const planetRouter = require('./routes/planets/planets.router');
 const cors = require('cors');
+const margan = require('morgan')
+
+const planetRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
+const morgan = require('morgan');
+
 const app = express();
+
 app.use(cors({
     origin:"http://localhost:3000"
 }))
+
+// app.use(morgan('combined'));
+
 app.use(express.json());
-app.use(planetRouter);
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+app.use('/planets',planetRouter);
+app.use('/launches', launchesRouter);
+
+app.get('/*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html' ))
+})
 
 module.exports = app;
