132fb0218dab4843776fb4006b2d43c6a5ed1e2d
ashadbubt <ashad.feni@gmail.com>
2022-08-29 02:45:14 +0600

    Mongo use

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,48 +1,91 @@
-const launches = new Map();
-let latestFlightNumber = 100;
+const launchesDatabase = require('./launches.mongo');
+const  planets = require('./planet.mongo');
+const DEFAULT_FLIGHT_NUMBER  = 100;
+
 const launch = {
     flightNumber:100,
     mission: "Keplar Exploration A",
     rocket: "Explorer IS1",
     launchDate:new Date('December 27 ,2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-442 b',
     customer:['ZTM', 'NASA'],
     upcoming:true,
     success:true
 }
+// target: 'Kepler-442 b',
+saveLaunchs(launch);
 
-launches.set(launch.flightNumber, launch);
-
-function getAllLaunches(){    
-    return Array.from(launches.values());
-    // return {a:10};
-    
+async function getAllLaunches(){    
+    return  await launchesDatabase
+    .find({}, {'_id':0, '__v':0});
 }
 
-function addNewLaunch(launch){
-    latestFlightNumber++;
-    launches.set(latestFlightNumber, Object.assign(launch,{
-        flightNumber:latestFlightNumber,        
+
+async function  getLatestFlightNumber(){
+    const latestLaunche = await launchesDatabase
+    .findOne()
+    .sort('-flightNumber');
+    if(!latestLaunche){
+        return DEFAULT_FLIGHT_NUMBER; 
+    }
+    return latestLaunche.flightNumber
+} 
+
+async  function saveLaunchs(launch){
+    const planet = await planets.findOneAndUpdate({
+        keplerName: launch.target
+    });
+
+    if(!planet){
+        throw new Error('No matching planet found');
+    }
+
+    await launchesDatabase.updateOne({
+        flightNumber:launch.flightNumber,
+    },launch, {
+        upsert: true
+    })
+}
+
+async function scheduleNewLaunche(){
+    const newFlightNumber =  await getLatestFlightNumber()+1;
+    const newLaunch = Object.assign(launch, {
+        success: true,
+        upcoming: true,
         customer:['Z to Mastr ', 'NASA'],
-        upcoming:true,
-        success:true
-    }));
+        flightNumber:newFlightNumber 
+    })
+
+    await saveLaunchs(newLaunch);
 }
 
-function existsLaunchWithId(launchId){
-     return launches.has(launchId);
+
+async function existsLaunchWithId(launchId){
+     return await launchesDatabase.findOne({
+        flightNumber: launchId
+     });
 }
 
-function abortlaunchById(launchId){
-    const aborted =  launches.get(launchId);
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted;
+async function abortlaunchById(launchId){
+   const aborted =  await launchesDatabase.updateOne({
+        flightNumber:launchId
+    }, {
+        upcoming: false,
+        success: false
+    })
+
+   return aborted.modifiedCount == 1; 
+
+
+    // const aborted =  launches.get(launchId);
+    // aborted.upcoming = false;
+    // aborted.success = false;
+    // return aborted;
 }
 
 module.exports = {
     getAllLaunches,
-    addNewLaunch,
     existsLaunchWithId,
+    scheduleNewLaunche,
     abortlaunchById
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,22 +1,19 @@
 const http = require('http');
 const app = require('./app') 
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 require('dotenv').config()
 
 const server = http.createServer(app);
+
 const PORT = process.env.PORT || 8000;
+
 async function  startServer(){
+    await mongoConnect();
     await loadPlanetsData();
     server.listen(PORT, ()=>{
         console.log(`Listinging Port ${ PORT } ...`);
     });
 }
 startServer();
-
-// const express = require('express');
-
-// const app = express();
-// app.listen();
-
-
 
